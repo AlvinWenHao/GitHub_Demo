153911b2d023f0069e6ececfec3f3c1c694a6fb5
zhwenhao <358403780@qq.com>
2021-05-08 10:59:40 +0800

    第三次提交：添加了一个方法

--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -1,5 +1,12 @@
 public class TestController {
     public static void main(String[] args) {
         System.out.println("cscfhnh");
+        System.out.println(1);
+        System.out.println(1);
+    }
+
+    //假设这个方法是项目中查询全部的方法
+    public void test(){
+
     }
 }
